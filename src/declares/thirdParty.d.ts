36cbb2fc1b49f3db9ed4b1739e122cd155db2828
fengyinchao <fengyinchao@shimo.im>
2018-11-24 15:01:52 +0800

    chores: 完成tslint prettier

---
+++ src/declares/thirdParty.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-native-default-preference';
+declare module 'react-native-elements';
+declare module 'react-native-splash-screen';
+
+
--- src/screen/Login/index.tsx
+++ src/screen/Login/index.tsx
@@ -1,77 +1,78 @@
-import React, { Component } from 'react';
-import { View, Text, KeyboardAvoidingView } from 'react-native';
-import { withNavigation } from 'react-navigation';
-import Preference from 'react-native-default-preference';
+import React, { Component } from "react"
+import { View, Text, KeyboardAvoidingView, ViewStyle } from "react-native"
+import { withNavigation } from "react-navigation"
+import Preference from "react-native-default-preference"
 import {
   FormLabel,
   FormInput,
   FormValidationMessage,
   Button
-  // @ts-ignore
-} from 'react-native-elements';
+} from "react-native-elements"
+import SplashScreen from "react-native-splash-screen"
 
-const initialState = {
-  account: '',
-  accountError: null,
-  password: '',
-  passwordError: null,
-  behavior: 'padding' || 'position' || ''
+enum Behavior {
+  Padding = "padding",
+  Position = "position"
 }
-const initProps = {
-  navigation:{
-    navigate:path=>{}
+interface State {
+  account: string
+  accountError: string
+  password: string
+  passwordError: string
+  behavior: Behavior
+}
+
+class List extends Component<{}, State> {
+  readonly state: State = {
+    account: "",
+    accountError: "",
+    password: "",
+    passwordError: "",
+    behavior: Behavior.Position
   }
-}
-type State = Readonly<typeof initialState>
-type Props = Readonly<typeof initProps>
 
-const a = 'position'
-const b = 'padding'
-const c = 'height'
-
-type test = typeof a | typeof b | typeof c;
-
-
-
-class List extends Component<Props,State> {
-
-  private readonly state: State = initialState
-
-  _onChangeTextAccount = account => {
+  _onChangeTextAccount = (account: string) => {
     this.setState({
       account,
-      accountError: null
-    });
-  };
-  _onChangeTextPassword = password => {
+      accountError: ""
+    })
+  }
+  _onChangeTextPassword = (password: string) => {
     this.setState({
       password,
-      passwordError: null
-    });
-  };
+      passwordError: ""
+    })
+  }
+
+  componentDidMount = () => {
+    SplashScreen.hide()
+  }
+
   _login = async () => {
-    const { account, password } = this.state;
+    const { account, password } = this.state
     if (!account) {
       this.setState({
-        accountError: '账号不能为空'
-      });
-      return;
+        accountError: "账号不能为空"
+      })
+
+      return
     }
     if (!password) {
       this.setState({
-        passwordError: '密码不能为空'
-      });
-      return;
+        passwordError: "密码不能为空"
+      })
+
+      return
     }
     await Preference.setMultiple({
       account,
       password
-    });
-    this.props.navigation.navigate('/tab-navigation');
-  };
+    })
+    // this.props.navigation.navigate("/tab-navigation")
+  }
   render() {
     return (
-      <KeyboardAvoidingView style={styles.body} behavior={(this.state.behavior as test)}>
+      <KeyboardAvoidingView style={styles.body} behavior={this.state.behavior}>
         <View>
           <Text style={styles.head}>欢迎来到RNDemo</Text>
         </View>
@@ -87,31 +88,26 @@ class List extends Component<Props,State> {
 
         <Button style={styles.buttonStyle} title="登陆" onPress={this._login} />
       </KeyboardAvoidingView>
-    );
+    )
   }
 }
-const StyleLint = {
-  body:{},
-  head:{},
-  buttonStyle:{}
-}
-type Style = typeof StyleLint
-const styles:Style = {
+
+const styles = {
   body: {
     flex: 1,
-    justifyContent: 'center'
-  },
+    justifyContent: "center"
+  } as ViewStyle,
   head: {
     marginTop: 20,
     height: 50,
     lineHeight: 50,
     fontSize: 20,
     marginBottom: 20,
-    textAlign: 'center'
-  },
+    textAlign: "center"
+  } as ViewStyle,
   buttonStyle: {
     marginTop: 20
-  }
-};
+  } as ViewStyle
+}
 
-export default withNavigation(List);
+export default withNavigation(List)
